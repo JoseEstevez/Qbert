0755dfb851ddf4948681af234774976a03f26e6d
JoseEstevez <jae9307@g.rit.edu>
2023-09-15 14:39:08 -0400

    Initial commit

--- qbert/qbert-play1.py
+++ qbert/qbert-play1.py
@@ -2,16 +2,87 @@ import argparse
 import sys
 import pdb
 import gymnasium as gym
+import numpy as np
 from gymnasium import wrappers, logger
 
+import time
+
+
 class Agent(object):
-    """The world's simplesst agent!"""
+    """The world's simplest agent!"""
+    beforeColor = np.array([45, 87, 176])
+    qbertColor = np.array([181, 83, 40])
+    coilyColor = np.array([146, 70, 192])
+    actions = []
+
     def __init__(self, action_space):
         self.action_space = action_space
 
     # You should modify this function
     def act(self, observation, reward, done):
+        vertInc = 30
+        horizInc = 14
+        self.actions = [5, 4, 3, 2]
+
+        if reward == 3100:
+            time.sleep(5)
+            beforeColor = observation[36][77]
+            self.beforeColor = beforeColor
+        else:
+            beforeColor = self.beforeColor
+
+        # get the location of the qbert
+        if np.where(observation == self.qbertColor)[0].size == 0:
+            return self.action_space.sample()
+
+        # 'top left' of qbert
+
+        # 'bottom right' of qbert
+        qbertcoord2 = [np.where(observation == self.qbertColor)[0][-1], np.where(observation == self.qbertColor)[1][-1]]
+
+        # look down and left for unflipped block
+
+        brightx = qbertcoord2[0] + vertInc
+        brighty = qbertcoord2[1] - horizInc
+        self.chooseMovement(brightx, brighty, 5)
+
+        # look down and right for unflipped block
+
+        brightx = qbertcoord2[0] + vertInc
+        brighty = qbertcoord2[1] + horizInc
+        self.chooseMovement(brightx, brighty, 3)
+        # look up and left for unflipped block
+
+        brightx = qbertcoord2[0] - vertInc
+        brighty = qbertcoord2[1] - horizInc
+        self.chooseMovement(brightx, brighty, 4)
+
+        # look up and right for unflipped block
+        brightx = qbertcoord2[0] - vertInc
+        brighty = qbertcoord2[1] + horizInc
+        self.chooseMovement(brightx, brighty, 2)
+
         return self.action_space.sample()
+
+    def chooseMovement(self, brightx, brighty, action):
+        topleft, botright = makeLookBox(np.array([brightx, brighty]))
+        try:
+            if ((observation[topleft[0]:botright[0], topleft[1]:botright[1]] == self.coilyColor).any() or
+                    (observation[topleft[0]:botright[0], topleft[1]:botright[1]] == np.array([0, 0, 0])).all()):
+                self.actions.remove(action)
+            elif (observation[topleft[0]:botright[0], topleft[1]:botright[1]] == self.beforeColor).any():
+                return action
+            else :
+                return np.random.choice(self.actions)
+        except IndexError:
+            pass
+
+
+def makeLookBox(coord):
+    topLeft = [coord[0] - 20, coord[1] - 10]
+    botRight = [coord[0] + 10, coord[1] + 10]
+
+    return topLeft, botRight
 
 ## YOU MAY NOT MODIFY ANYTHING BELOW THIS LINE OR USE
 ## ANOTHER MAIN PROGRAM
@@ -32,7 +103,6 @@ if __name__ == '__main__':
     # like: tempfile.mkdtemp().
     outdir = 'random-agent-results'
 
-
     env.unwrapped.seed(0)
     agent = Agent(env.action_space)
 
@@ -43,16 +113,14 @@ if __name__ == '__main__':
     special_data = {}
     special_data['ale.lives'] = 3
     observation = env.reset()[0]
-    
 
     while not terminated:
-        
         action = agent.act(observation, reward, terminated)
-        #pdb.set_trace()
+        # pdb.set_trace()
         observation, reward, terminated, truncated, info = env.step(action)
         score += reward
         env.render()
-     
+
     # Close the env and write monitor result info to disk
-    print ("Your score: %d" % score)
+    print("Your score: %d" % score)
     env.close()
