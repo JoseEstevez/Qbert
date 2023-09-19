bebf65be4bad942ab29a1ce57442b2039e52721b
Brett Lubberts <bpl9915@g.rit.edu>
2023-09-19 15:03:17 -0400

    Improved on getting stuck in corners

--- qbert/qbert-play1.py
+++ qbert/qbert-play1.py
@@ -10,45 +10,52 @@ import time
 
 class Agent(object):
     """The world's simplest agent!"""
-    beforeColor = np.array([45, 87, 176])
-    qbertColor = np.array([181, 83, 40])
-    coilyColor = np.array([146, 70, 192])
+    initBeforeColor = False
+    initQbertColor = False
+    beforeColor = 0
+    qbertColor = np.dot(np.array([181, 83, 40]), [0.299, 0.587, 0.114])
+    coilyColor = np.dot(np.array([146, 70, 192]), [0.299, 0.587, 0.114])
     defaultActions = []
     preferredActions = []
-    lastReward = 0.0
     lvlchange = 0
+    grayscale = np.zeros((210, 160))
 
     def __init__(self, action_space):
         self.action_space = action_space
 
-    # You should modify this function
     def act(self, observation, reward, done):
+        self.grayscale = np.dot(observation[:, :, :3], [0.299, 0.587, 0.114])
+
+        # initialize the color for the first time
+        if not self.initBeforeColor:
+            self.beforeColor = self.grayscale[92][77]
+            self.initBeforeColor = True
+
         vertInc = 30
         horizInc = 14
         self.defaultActions = [5, 4, 3, 2]
         self.preferredActions = []
 
+        print(self.beforeColor)
+
         if reward == 100:
-            # beforeColor = observation[92][77]
-            # self.beforeColor = observation[92][77]
-            # self.lastReward = reward
-            self.lvlchange = 2
+            self.lvlchange = 3
             return 0
         elif self.lvlchange > 0:
-            self.beforeColor = observation[92][77]
+            self.beforeColor = self.grayscale[92][77]
             self.lvlchange -= 1
+            return 0
 
-        # self.lastReward = reward
-        # get the location of the qbert
-        if np.where(observation == self.qbertColor)[0].size == 0:
-            return self.action_space.sample()
-
-        # 'top left' of qbert
+        # if Qbert is not on screen, do nothing
+        if np.where(self.grayscale == self.qbertColor)[0].size == 0:
+            return 0
 
         # 'bottom right' of qbert
-        qbertcoord = [np.where(observation == self.qbertColor)[0][-1], np.where(observation == self.qbertColor)[1][-1]]
+        qbertcoord = [np.where(self.grayscale == self.qbertColor)[0][-1],
+                      np.where(self.grayscale == self.qbertColor)[1][-1]]
 
-        # Check if Coily is at any 2 block distance from Qbert
+        # Check if Coily is at any horizontal or vertical 2 block distance
+        # from Qbert
 
         upx = qbertcoord[0] - 2 * vertInc
         upy = qbertcoord[1]
@@ -62,47 +69,41 @@ class Agent(object):
         rightx = qbertcoord[0]
         righty = qbertcoord[1] + 2 * horizInc
 
-        if (self.checkCoilyNear(upx, upy) or self.checkCoilyNear(downx, downy) or
-                self.checkCoilyNear(leftx, lefty) or self.checkCoilyNear(rightx, righty)):
+        if (self.checkCoilyNear(upx, upy) or self.checkCoilyNear(downx,
+                                                                 downy) or
+                self.checkCoilyNear(leftx, lefty) or self.checkCoilyNear(
+                    rightx, righty)):
             return 0
 
         # look down and left for unflipped block
 
-        # nearx = qbertcoord[0] + vertInc
-        # neary = qbertcoord[1] - horizInc
-        # farx = qbertcoord[0] + 2 * vertInc
-        # fary = qbertcoord[1] - 2 * horizInc
         near = np.array([qbertcoord[0] + vertInc, qbertcoord[1] - horizInc])
-        far = np.array([qbertcoord[0] + 2 * vertInc, qbertcoord[1] - 2 * horizInc])
+        far = np.array(
+            [qbertcoord[0] + 2 * vertInc, qbertcoord[1] - 2 * horizInc])
         self.chooseMovement(near, far, 5)
 
         # look down and right for unflipped block
 
-        # nearx = qbertcoord[0] + vertInc
-        # neary = qbertcoord[1] + horizInc
-        # farx = qbertcoord[0] + 2 * vertInc
-        # fary = qbertcoord[1] + 2 * horizInc
         near = np.array([qbertcoord[0] + vertInc, qbertcoord[1] + horizInc])
-        far = np.array([qbertcoord[0] + 2 * vertInc, qbertcoord[1] + 2 * horizInc])
+        far = np.array(
+            [qbertcoord[0] + 2 * vertInc, qbertcoord[1] + 2 * horizInc])
         self.chooseMovement(near, far, 3)
+
         # look up and left for unflipped block
 
-        # nearx = qbertcoord[0] - vertInc
-        # neary = qbertcoord[1] - horizInc
-        # farx = qbertcoord[0] - 2 * vertInc
-        # fary = qbertcoord[1] - 2 * horizInc
         near = np.array([qbertcoord[0] - vertInc, qbertcoord[1] - horizInc])
-        far = np.array([qbertcoord[0] - 2 * vertInc, qbertcoord[1] - 2 * horizInc])
+        far = np.array(
+            [qbertcoord[0] - 2 * vertInc, qbertcoord[1] - 2 * horizInc])
         self.chooseMovement(near, far, 4)
 
         # look up and right for unflipped block
-        # nearx = qbertcoord[0] - vertInc
-        # neary = qbertcoord[1] + horizInc
-        # farx = qbertcoord[0] - 2 * vertInc
-        # fary = qbertcoord[1] + 2 * horizInc
+
         near = np.array([qbertcoord[0] - vertInc, qbertcoord[1] + horizInc])
-        far = np.array([qbertcoord[0] - 2 * vertInc, qbertcoord[1] + 2 * horizInc])
+        far = np.array(
+            [qbertcoord[0] - 2 * vertInc, qbertcoord[1] + 2 * horizInc])
         self.chooseMovement(near, far, 2)
+
+        # do preferred action if there is one, otherwise do safe default action
 
         if (len(self.preferredActions) != 0):
             return np.random.choice(self.preferredActions)
@@ -114,27 +115,52 @@ class Agent(object):
         tlnear, brnear = makeLookBox(near)
         tlfar, brfar = makeLookBox(far)
 
-        # we need to check the color of qberts current condition, then look to the look box and see if there's some color
-        # there that isn't in qberts current box, do it after the first if because it's more important to check that coily
-        # isn't there first, somehow put it in the elif.
-        # box = observation[tlnear[0]:brnear[0], tlnear[1]:brnear[1]]
-        # box.reshape((brnear[0] - tlnear[0]) * (brnear[1] - tlnear[1]), 3)
-        # colors = tuple
-
         try:
-            if ((observation[tlnear[0]:brnear[0], tlnear[1]:brnear[1]] == self.coilyColor).any() or
+
+            # if the action is not safe, remove it from the list of default
+            # actions, otherwise see if it is preferred
+            if ((self.grayscale[tlnear[0]:brnear[0], tlnear[1]:brnear[1]] == self.coilyColor).any() or
                     self.isEmpty(observation, tlnear, brnear) or
-                    (observation[tlfar[0]:brfar[0], tlfar[1]:brfar[1]] == self.coilyColor).any()):
+                    (self.grayscale[tlfar[0]:brfar[0], tlfar[1]:brfar[1]] == self.coilyColor).any()):
                 self.defaultActions.remove(action)
-            elif (observation[tlnear[0]:brnear[0], tlnear[1]:brnear[1]] == self.beforeColor).any():
+            elif (self.grayscale[tlnear[0]:brnear[0],
+                  tlnear[1]:brnear[1]] == self.beforeColor).any():
                 self.preferredActions.append(action)
+
+            # stop traps
+            # stop trap in bottom right
+            if action == 3 and brnear[1] > 140 and brnear[0] < 210:
+                # check up and left
+                tmptl = [tlnear[0] - 60, tlnear[1] - 28]
+                tmpbr = [brnear[0] - 60, brnear[1] - 28]
+                # check down and left
+                tmptl2 = [tlnear[0], tlnear[1] - 14]
+                tmpbr2 = [brnear[0], brnear[1] - 14]
+                if ((self.grayscale[tmptl[0]:tmpbr[0], tmptl[1]:tmpbr[1]] == self.coilyColor).any() or
+                        (self.grayscale[tmptl2[0]:tmpbr2[0], tmptl2[1]:tmpbr2[1]] == self.coilyColor).any()):
+                    if action in self.defaultActions:
+                        self.defaultActions.remove(action)
+            # stop trap in bottom left
+            elif action == 5 and tlnear[1] < 10 and brnear[0] < 210:
+                # check up and right
+                tmptl = [tlnear[0] - 60, tlnear[1] + 28]
+                tmpbr = [brnear[0] - 60, brnear[1] + 28]
+                # check down and right
+                tmptl2 = [tlnear[0], tlnear[1] + 14]
+                tmpbr2 = [brnear[0], brnear[1] + 14]
+                if ((self.grayscale[tmptl[0]:tmpbr[0], tmptl[1]:tmpbr[1]] == self.coilyColor).any() or
+                        (self.grayscale[tmptl2[0]:tmpbr2[0], tmptl2[1]:tmpbr2[1]] == self.coilyColor).any()):
+                    if action in self.defaultActions:
+                        self.defaultActions.remove(action)
+
         except IndexError:
             pass
 
     def isEmpty(self, observation, topleft, botright):
-        grayscale = np.mean(observation, axis=2)
 
-        colors = np.unique(grayscale[topleft[0]:botright[0], topleft[1]:botright[1]])
+        grayscale = np.mean(observation, axis=2)
+        colors = np.unique(
+            grayscale[topleft[0]:botright[0], topleft[1]:botright[1]])
 
         if (colors.size > 2):
             return False
@@ -145,7 +171,8 @@ class Agent(object):
         topleft, botright = makeLookBox(np.array([x, y]))
         if x < 0 or y < 0:
             return False
-        if (observation[topleft[0]:botright[0], topleft[1]:botright[1]] == self.coilyColor).any():
+        if (self.grayscale[topleft[0]:botright[0],
+            topleft[1]:botright[1]] == self.coilyColor).any():
             return True
         return False
 
@@ -157,11 +184,12 @@ def makeLookBox(coord):
     return topLeft, botRight
 
 
-## YOU MAY NOT MODIFY ANYTHING BELOW THIS LINE OR USE
-## ANOTHER MAIN PROGRAM
+# YOU MAY NOT MODIFY ANYTHING BELOW THIS LINE OR USE
+# ANOTHER MAIN PROGRAM
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('--env_id', nargs='?', default='Qbert', help='Select the environment to run')
+    parser.add_argument('--env_id', nargs='?', default='Qbert',
+                        help='Select the environment to run')
     args = parser.parse_args()
 
     # You can set the level to logger.DEBUG or logger.WARN if you
