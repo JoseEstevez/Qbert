f790992156e27550de4739db03e6e8abfc0132ee
Brett Lubberts <bpl9915@g.rit.edu>
2023-09-19 15:32:55 -0400

    Fixed the issue with deep-fry guy

--- qbert/qbert-play1.py
+++ qbert/qbert-play1.py
@@ -1,3 +1,10 @@
+"""qbert-play1.py
+
+AI agent fof Qbert
+"""
+
+__author__ = "Brett Lubberts, Jose Estevez"
+
 import argparse
 import sys
 import pdb
@@ -11,7 +18,6 @@ import time
 class Agent(object):
     """The world's simplest agent!"""
     initBeforeColor = False
-    initQbertColor = False
     beforeColor = 0
     qbertColor = np.dot(np.array([181, 83, 40]), [0.299, 0.587, 0.114])
     coilyColor = np.dot(np.array([146, 70, 192]), [0.299, 0.587, 0.114])
@@ -21,10 +27,20 @@ class Agent(object):
     grayscale = np.zeros((210, 160))
 
     def __init__(self, action_space):
+        """Initialize the agent"""
         self.action_space = action_space
 
     def act(self, observation, reward, done):
-        self.grayscale = np.dot(observation[:, :, :3], [0.299, 0.587, 0.114])
+        """Choose an action for the agent
+
+        Keyword arguments:
+        observation -- the current observation
+        reward -- the reward from the last action
+        done -- whether the episode is over
+        """
+        # make the observation grayscale for easier manipulation
+        self.grayscale = np.dot(observation[:, :, :3],
+                                [0.299, 0.587, 0.114])
 
         # initialize the color for the first time
         if not self.initBeforeColor:
@@ -36,12 +52,10 @@ class Agent(object):
         self.defaultActions = [5, 4, 3, 2]
         self.preferredActions = []
 
-        print(self.beforeColor)
-
         if reward == 100:
-            self.lvlchange = 3
+            self.lvlchange += 1
             return 0
-        elif self.lvlchange > 0:
+        elif self.lvlchange > 1:
             self.beforeColor = self.grayscale[92][77]
             self.lvlchange -= 1
             return 0
@@ -69,9 +83,8 @@ class Agent(object):
         rightx = qbertcoord[0]
         righty = qbertcoord[1] + 2 * horizInc
 
-        if (self.checkCoilyNear(upx, upy) or self.checkCoilyNear(downx,
-                                                                 downy) or
-                self.checkCoilyNear(leftx, lefty) or self.checkCoilyNear(
+        if (self.checkCoilyNear(upx, upy) or self.checkCoilyNear(downx, downy)
+                or self.checkCoilyNear(leftx, lefty) or self.checkCoilyNear(
                     rightx, righty)):
             return 0
 
@@ -112,6 +125,13 @@ class Agent(object):
         return 0
 
     def chooseMovement(self, near, far, action):
+        """Choose a movement based on the near and far blocks
+
+        Keyword arguments:
+        near -- the block that is one block away from Qbert
+        far -- the block that is two blocks away from Qbert
+        action -- the action that corresponds to the movement
+        """
         tlnear, brnear = makeLookBox(near)
         tlfar, brfar = makeLookBox(far)
 
@@ -119,9 +139,11 @@ class Agent(object):
 
             # if the action is not safe, remove it from the list of default
             # actions, otherwise see if it is preferred
-            if ((self.grayscale[tlnear[0]:brnear[0], tlnear[1]:brnear[1]] == self.coilyColor).any() or
+            if ((self.grayscale[tlnear[0]:brnear[0],
+                 tlnear[1]:brnear[1]] == self.coilyColor).any() or
                     self.isEmpty(observation, tlnear, brnear) or
-                    (self.grayscale[tlfar[0]:brfar[0], tlfar[1]:brfar[1]] == self.coilyColor).any()):
+                    (self.grayscale[tlfar[0]:brfar[0],
+                     tlfar[1]:brfar[1]] == self.coilyColor).any()):
                 self.defaultActions.remove(action)
             elif (self.grayscale[tlnear[0]:brnear[0],
                   tlnear[1]:brnear[1]] == self.beforeColor).any():
@@ -136,8 +158,10 @@ class Agent(object):
                 # check down and left
                 tmptl2 = [tlnear[0], tlnear[1] - 14]
                 tmpbr2 = [brnear[0], brnear[1] - 14]
-                if ((self.grayscale[tmptl[0]:tmpbr[0], tmptl[1]:tmpbr[1]] == self.coilyColor).any() or
-                        (self.grayscale[tmptl2[0]:tmpbr2[0], tmptl2[1]:tmpbr2[1]] == self.coilyColor).any()):
+                if ((self.grayscale[tmptl[0]:tmpbr[0],
+                     tmptl[1]:tmpbr[1]] == self.coilyColor).any() or
+                        (self.grayscale[tmptl2[0]:tmpbr2[0],
+                         tmptl2[1]:tmpbr2[1]] == self.coilyColor).any()):
                     if action in self.defaultActions:
                         self.defaultActions.remove(action)
             # stop trap in bottom left
@@ -148,8 +172,10 @@ class Agent(object):
                 # check down and right
                 tmptl2 = [tlnear[0], tlnear[1] + 14]
                 tmpbr2 = [brnear[0], brnear[1] + 14]
-                if ((self.grayscale[tmptl[0]:tmpbr[0], tmptl[1]:tmpbr[1]] == self.coilyColor).any() or
-                        (self.grayscale[tmptl2[0]:tmpbr2[0], tmptl2[1]:tmpbr2[1]] == self.coilyColor).any()):
+                if ((self.grayscale[tmptl[0]:tmpbr[0],
+                     tmptl[1]:tmpbr[1]] == self.coilyColor).any() or
+                        (self.grayscale[tmptl2[0]:tmpbr2[0],
+                         tmptl2[1]:tmpbr2[1]] == self.coilyColor).any()):
                     if action in self.defaultActions:
                         self.defaultActions.remove(action)
 
@@ -157,6 +183,13 @@ class Agent(object):
             pass
 
     def isEmpty(self, observation, topleft, botright):
+        """Check if a block is empty
+
+        Keyword arguments:
+        observation -- the current observation
+        topleft -- the top left corner of the block
+        botright -- the bottom right corner of the block
+        """
 
         grayscale = np.mean(observation, axis=2)
         colors = np.unique(
@@ -168,6 +201,12 @@ class Agent(object):
         return True
 
     def checkCoilyNear(self, x, y):
+        """Check if Coily is at a certain block
+
+        Keyword arguments:
+        x -- the x coordinate of the block
+        y -- the y coordinate of the block
+        """
         topleft, botright = makeLookBox(np.array([x, y]))
         if x < 0 or y < 0:
             return False
@@ -178,6 +217,11 @@ class Agent(object):
 
 
 def makeLookBox(coord):
+    """Make a box around a block to look at
+
+    Keyword arguments:
+    coord -- the coordinate of the block
+    """
     topLeft = [coord[0] - 20, coord[1] - 10]
     botRight = [coord[0] + 10, coord[1] + 10]
 
